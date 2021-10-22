6abfe6fdcbcce77ceb07b3e62706dac91a0ebb37
tckz <at.tckz@gmail.com>
2021-10-22 14:41:02 +0900

    issue-id-token

---
+++ cmd/issue-id-token/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/api/idtoken"
+)
+
+// https://cloud.google.com/run/docs/authenticating/service-to-service
+// 指定audienceでIDトークン発行
+// 発行する権限はGOOGLE_APPLICATION_CREDENTIALSなどで指定
+
+var (
+	optAudience = flag.String("audience", "", "aud of id token to be issued")
+)
+
+func main() {
+	godotenv.Load()
+
+	flag.Parse()
+
+	if *optAudience == "" {
+		log.Fatalf("*** --audience must be specified")
+	}
+
+	ts, err := idtoken.NewTokenSource(context.Background(), *optAudience)
+	if err != nil {
+		log.Fatalf("idtoken.NewTokenSource: %v", err)
+	}
+
+	token, err := ts.Token()
+	if err != nil {
+		log.Fatalf("ts.Token: %v", err)
+	}
+
+	fmt.Println(token.AccessToken)
+}
